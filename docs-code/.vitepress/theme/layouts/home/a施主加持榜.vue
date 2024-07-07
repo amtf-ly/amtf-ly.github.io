ab28d3b2eddadaa52e466b6ce51082aa42d3a572
amtf <22831090@qq.com>
2024-07-07 19:38:19 +0800

    kk

--- docs-code/.vitepress/theme/layouts/home/a施主加持榜.vue
+++ docs-code/.vitepress/theme/layouts/home/a施主加持榜.vue
@@ -1,39 +1,40 @@
 <template>
   <transition appear enter-active-class="animated zoomIn" leave-active-class="animated zoomOut absolute-top">
     <div>
-      <q-banner :class="[' text-center 上圆角', 'q-list--bordered']">
-        <span class="小字">{{ 标题 }} </span>
-      </q-banner>
+      <!-- {{ data }} -->
+      <q-table class="表头色" title="" :rows="data" :columns="columns" row-key="_id" :rows-per-page-options="[10,20,100]">
+        <template v-slot:top>
+          <div class="text-center q-table__title full-width text-primary">{{ 标题 }}</div>
+          <!-- <q-space></q-space> -->
+          <!-- <q-btn color="positive " icon="add" dense size="sm" label="新增" @click="store.add()" outline /> -->
+        </template>
+        <template v-slot:header="props">
+          <q-tr :props="props">
+            <!-- {{ props.cols }} -->
+            <!-- <q-th auto-width /> -->
+            <q-th v-for="col in props.cols" :key="col.name" :props="props" class="text-primary">
+              {{ col.label }}
+            </q-th>
+          </q-tr>
+        </template>
+        <template v-slot:body="props">
+          <q-tr :props="props">
 
-      <q-table class="表头色" title="柜体柜门类" :rows="data" :columns="columns" row-key="id" binary-state-sort
-          :rows-per-page-options="[0]" separator="cell" hide-bottom>
-          <template v-slot:top>
-            <div class=" q-table__title">柜体柜门类</div>
-            <q-space></q-space>
-            <!-- <q-btn color="positive " icon="add" dense size="sm" label="新增" @click="store.add()" outline /> -->
-
-          </template>
-          <template v-slot:header="props">
-            <q-tr :props="props">
-              <q-th auto-width />
-              <q-th v-for="col in props.cols" :key="col.name" :props="props">
-                {{ col.label }}
-              </q-th>
-            </q-tr>
-          </template>
-          <template v-slot:body="props">
-            <q-tr :props="props">
-              <!-- <q-td auto-width class="q-gutter-xs">
-                <q-btn color="positive " icon="zoom_in" dense @click="store.update(props.row.id)" size="sm" outline />
-                <q-btn color="negative " icon="clear" dense @click="store.删除主表行(props.row.id)" size="sm" outline />
-              </q-td> -->
-              <q-td v-for="col in props.cols" :key="col.name" :props="props">
+            <q-td v-for="col in props.cols" :key="col.name" :props="props">
+              <!-- {{ col }} -->
+              <template v-if="col.name == '充电金额'">
+                <q-badge color="orange" class="a4">
+                  {{ col.value }}
+                </q-badge>
+              </template>
+              <template v-else>
                 {{ col.value }}
-              </q-td>
-            </q-tr>
-          </template>
+              </template>
+            </q-td>
+          </q-tr>
+        </template>
 
-        </q-table>
+      </q-table>
 
     </div>
   </transition>
@@ -41,35 +42,69 @@
 
 <script setup>
 import { withBase } from 'vitepress'
-
+import { date } from 'quasar'
 const props = defineProps(['data', "标题"])
 // console.log('props.data :>> ', props.data);
 // const kk= props
-import A进度条 from './A进度条.vue';
 import { computed } from 'vue';
-import { useDocStore } from "../store/index.js";
-const docStore = useDocStore();
-const 背景色 = computed(() => {
-  // docStore.$q.dark.isActive === false ? "true" : "false"
-  const obj = {}
-  // console.log('props :>> ', props);
-  // console.log('props.data :>> ', props.data);
-  // const kk= props
 
-  // data是axios异步获取的，可能为空
-  try {
-    (props.data).forEach(currentItem => {
-      // console.log('currentItem :>> ', currentItem);
-      // obj[currentItem.名称]=currentItem.完成 ? 'bg-green-1' : 'bg-orange-1'
-      const 主色 = currentItem.完成 ? 'green' : 'orange'
-      const 深度 = docStore.$q.dark.isActive ? "5" : "1"
-      obj[currentItem.名称] = `bg-${主色}-${深度}`
-    })
-  } catch (error) { }
+// import { useDocStore } from "../store/index.js";
+// const docStore = useDocStore();
 
-  // console.log('obj :>> ', obj);
-  return obj
-}
+const columns = [
+  {
+    name: "用户",
+    label: "用户",
+    align: "center",
+    field: row => row.user_id?.[0]?.username,
+    sortable: true,
+  },
+  {
+    name: "充电金额",
+    label: "充电金额",
+    align: "center",
+    field: row => row.chongdianjine / 100,
+    sortable: true,
+  },
+  {
+    name: "注册日期",
+    label: "注册日期",
+    align: "center",
+    field: row => date.formatDate(row.user_id?.[0].register_date, 'YYYY-MM-DD'),
+    sortable: true,
+  },
+  {
+    name: "使用的程序",
+    label: "使用的程序",
+    align: "center",
+    field: row => {
+      const apps = row.appid_id?.map(item => {
+        return item.appname
+      })
+      return apps?.join(',')
+    },
+    sortable: true,
+  },
 
-);
+  // {
+  //   name: "单位",
+  //   label: "单位",
+  //   align: "center",
+  //   field: "单位",
+  //   sortable: true,
+  // },
+  // {
+  //   name: "价格",
+  //   label: "价格￥",
+  //   align: "center",
+  //   field: "价格",
+  //   sortable: true,
+  // },
+]
 </script>
+
+<style lang="scss" scoped>
+.a4{
+  font-size: 16px;
+}
+</style>
